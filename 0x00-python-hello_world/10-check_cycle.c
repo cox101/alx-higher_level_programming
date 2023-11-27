8f0c5db726f07d643607e682fc5a298bc523ecc8
cox101 <cox2659@gmail.com>
2023-11-27 13:13:16 -0800

    Initial commit

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * check_cycle - Checks if a singly-linked list contains a cycle.
@@ -8,25 +9,60 @@
  * Return: If there is no cycle - 0.
  *         If there is a cycle - 1.
  */
-int check_cycle(listint_t *list)
-{
-	listint_t *slow, *fast;
+/* Definition for singly-linked list. */
+struct listint_s {
+    int n;
+    struct listint_s *next;
+};
 
-	if (list == NULL || list->next == NULL)
-		return (0);
+typedef struct listint_s listint_t;
 
-	slow = list->next;
-	fast = list->next->next;
+/**
+ * check_cycle - Checks if a singly linked list has a cycle.
+ * @list: Pointer to the head of the linked list.
+ * Return: 1 if there is a cycle, 0 otherwise.
+ */
+int check_cycle(listint_t *list) {
+    listint_t *slow = list, *fast = list;
 
-	while (slow && fast && fast->next)
-	{
-		if (slow == fast)
-			return (1);
+    while (slow && fast && fast->next) {
+        slow = slow->next;        /* Move one step at a time */
+        fast = fast->next->next;  /* Move two steps at a time */
 
-		slow = slow->next;
-		fast = fast->next->next;
-	}
+        if (slow == fast) {
+            /* If there is a cycle, slow and fast will meet */
+            return 1;
+        }
+    }
 
-	return (0);
+    /* If we reach here, there is no cycle */
+    return 0;
 }
 
+/* Example usage */
+int main(void) {
+    listint_t *head, *node;
+
+    /* Create a linked list with a cycle */
+    head = malloc(sizeof(listint_t));
+    head->n = 1;
+    head->next = NULL;
+
+    node = malloc(sizeof(listint_t));
+    node->n = 2;
+    node->next = head;
+    head->next = node;
+
+    /* Check if there is a cycle */
+    if (check_cycle(head)) {
+        printf("There is a cycle in the linked list.\n");
+    } else {
+        printf("There is no cycle in the linked list.\n");
+    }
+
+    /* Free memory */
+    free(head);
+    free(node);
+
+    return 0;
+}
