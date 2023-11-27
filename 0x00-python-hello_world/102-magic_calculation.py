7267c1fdd2b7970925941cce9ee36bafc0170610
cox101 <cox2659@gmail.com>
2023-11-27 12:57:10 -0800

    Initial commit

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python3
+import dis
+
 def magic_calculation(a, b):
-    result = 98 ** a
-    result += b
+    result = a ** b
+    result += 98
     return result
+
+/*Compare bytecode*/
+dis.dis(magic_calculation)
 
