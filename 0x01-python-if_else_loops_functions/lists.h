3f55bcf4000b7270620261a898d50ac120aea97c
cox101 <cox2659@gmail.com>
2023-11-28 13:00:52 -0800

    Initial commit

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -2,17 +2,18 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/* Definition of the structure */
 typedef struct listint_s {
     int n;
     struct listint_s *next;
 } listint_t;
 
-/* Function prototype */
+
 listint_t *insert_node(listint_t **head, int number);
 
-/* Other function prototypes if needed */
+
 
 #endif /* LISTS_H */
 
