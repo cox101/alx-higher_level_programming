89c4a3042868a60f9aeece104287e8f8234f7706
cox101 <cox2659@gmail.com>
2023-11-28 13:07:37 -0800

    Initial commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for char_code in range(ord('z'), ord('a') - 1, -1):
+for char_code in range(ord('z'), ord('a') - 1, -2):
     print("{}".format(chr(char_code)), end="")
     if char_code % 2 == 0:
         print("{}".format(chr(char_code - ord('a') + ord('A'))), end="")
