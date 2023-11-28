0e93aee0abd84652b4321c7a80438037f2574353
cox101 <cox2659@gmail.com>
2023-11-28 12:12:46 -0800

    Initial commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def uppercase(input_str):
-    for char in input_str:
-        if 'a' <= char <= 'z':
-            char = chr(ord(char) - ord('a') + ord('A'))
-        print(char, end="")
-    print()
-
-uppercase("Hello, World!")
-
+def uppercase(str):
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            char = chr(ord(c) - 32)
+        else:
+            char = c
+        print("{:s}".format(char), end="")
+    print('')
