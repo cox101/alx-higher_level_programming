09beed5aaca966d49011fe73c391696c5c4ff42e
cox101 <cox2659@gmail.com>
2023-11-30 11:23:50 -0800

    Initial commit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, multiply, divide
-a = 10
-b = 5
-result_add = add(a, b)
-result_subtract = subtract(a, b)
-result_multiply = multiply(a, b)
-result_divide = divide(a, b)
+if __name__ == "__main__":
+    from calculator_1 import add, subtract, multiple, division
 
-print(f"The sum of {a} and {b} is: {result_add}")
-print(f"The difference between {a} and {b} is: {result_subtract}")
-print(f"The product of {a} and {b} is: {result_multiply}")
-print(f"The division of {a} by {b} is: {result_divide}")
+    a = 10
+    b = 5
 
+    result_add = add(a, b)
+    result_sub = subtract(a, b)
+    result_mul = multiple(a, b)
+    result_div = division(a, b)
 
+    print("{} + {} = {}".format(a, b, result_add))
+    print("{} - {} = {}".format(a, b, result_subtract))
+    print("{} * {} = {}".format(a, b, result_multiple))
+    print("{} / {} = {}".format(a, b, result_division))
