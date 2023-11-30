a37990cfb77a9ce63c63ff86ddce1ec234d473a1
cox101 <cox2659@gmail.com>
2023-11-30 11:51:43 -0800

    Initial commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-import sys
-import types
-
-def print_module_names():
-    module = types.ModuleType("hidden_4")
-    with open("hidden_4.pyc", "rb") as file:
-        magic = file.read(4)
-        timestamp = file.read(4)
-        code_object = compile(file.read(), "hidden_4.pyc", "exec")
-        sys.modules["hidden_4"] = module
-        exec(code_object, module.__dict__)
-
-    names = sorted(name for name in dir(module) if not name.startswith("__"))
-    for name in names:
-        print(name)
-
 if __name__ == "__main__":
-    print_module_names()
-
+    from hidden_4 import *
+    arr = dir()
+    for i in range(0, len(arr)):
+        if arr[i][0:2] != "__":
+            print("{}".format(arr[i]))
