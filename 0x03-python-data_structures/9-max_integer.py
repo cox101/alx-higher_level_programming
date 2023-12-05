391347abc7f5f036af57288557ec7273f7ece6b5
cox101 <cox2659@gmail.com>
2023-12-05 11:59:12 -0800

    Initial commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if not sentence:
-        return (0, None)
-    else:
-        return (len(sentence), sentence[0])
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    max_num = my_list[0]
+    for num in my_list:
+        if num > max_num:
+            max_num = num
+
+    return max_num
