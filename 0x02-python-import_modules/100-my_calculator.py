b59462948058d800d997dc47f1795c80b0878cb4
cox101 <cox2659@gmail.com>
2023-12-06 02:55:30 -0800

    Initial commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, multiply, divide
 import sys
+from calculator_1 import add, subtract, multiply, divide
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./your_program.py <a> <operator> <b>")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    result = None
-
+def calculate(a, operator, b):
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
@@ -25,5 +15,15 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+    print(f"{a} {operator} {b} = {result}")
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    calculate(a, operator, b)
