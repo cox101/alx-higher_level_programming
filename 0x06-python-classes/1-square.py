5ef395baf40a9712336cd3c4a5a10c5dd122adfb
cox101 <cox2659@gmail.com>
2023-12-19 11:31:47 -0800

    Initial commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 class Square:
+    """Class that defines a square with a private size attribute."""
+    
     def __init__(self, size):
+        """Instantiation with size (no type/value verification)."""
         self.__size = size
 
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+class Square:
+    """Class that defines a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        """
+        Instantiation with optional size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Args:
+            value (int): The size value to set.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Public method to calculate the area of the square."""
+        return self.__size ** 2
+
