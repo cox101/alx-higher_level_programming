6ffba208c7fbfb9ebe687859a05f74dcf20a2364
cox101 <cox2659@gmail.com>
2024-01-08 13:14:16 -0800

    Initial commit

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
 """Defines a class Rectangle that inherits from BaseGeometry."""
 
-class BaseGeometry:
-    def area(self):
-        raise Exception("area() is not implemented")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
+    """Represents a rectangle.
+    Private instance attributes:
+        - width
+        - height
+    Public method area().
+    Inherits from BaseGeometry.
+    """
+
     def __init__(self, width, height):
+        """Initializes an instance.
+        Args:
+            - width: width of the rectangle
+            - heigth: height of the rectangle
+        """
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
-    def area(self):
-        """Return the area of the rectangle."""
-        return self.__width * self.__height
-
     def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        """Returns a formatted string."""
 
-    def print(self):
-        print(self.__str__())
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
 
+    def area(self):
+        """Computes the area of the Rectangle instance.
+        Overwrites the area() method from BaseGeometry.
+        """
+
+        return self.__width * self.__height
