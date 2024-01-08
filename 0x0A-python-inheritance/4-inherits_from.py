eb41a241967ce08ad816b16a387a20871e7754dd
cox101 <cox2659@gmail.com>
2024-01-08 12:21:15 -0800

    Initial commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,4 +3,14 @@
 
 def is_inherited_from_class(obj, a_class):
     """Check if the object is an instance of a class inherited (directly or indirectly) from the specified class."""
-    return issubclass(type(obj), a_class)
+    """
+    Checks if obj is an instance of a class inherited from a_class.
+    
+    Args:
+        obj: The object to evaluate.
+        a_class: The class to check against.
+    
+    Returns:
+        True if obj is an instance of a class inherited from a_class, False otherwise.
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
