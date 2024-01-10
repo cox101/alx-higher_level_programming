8ee2aa8b3b547f460d6dcc17b9ae0931074d828d
cox101 <cox2659@gmail.com>
2024-01-10 11:51:53 -0800

    Initial commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,35 +1,16 @@
-#!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list and saves them to a file.
-"""
+#!/usr/bin/python3 
+"""a script that adds all arguments to a Python list, and then save them to a file """
 
 import sys
-import json
-
-def save_to_json_file(my_list, filename):
-    with open(filename, mode='w', encoding='utf-8') as file:
-        json.dump(my_list, file)
 
 if __name__ == "__main__":
-    """Check if at least one argument is provided"""
-    if len(sys.argv) < 2:
-        print("Usage: {} arg1 arg2 ...".format(sys.argv[0]))
-        sys.exit(1)
-     """Extract command-line arguments"""
-    arguments = sys.argv[1:]
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-    """ Load existing list or create a new one"""
     try:
-        with open("add_item.json", mode='r', encoding='utf-8') as file:
-            my_list = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        my_list = []
-
-    """ Add arguments to the list"""
-    my_list.extend(arguments)
-
-    """Save the updated list to the file"""
-    save_to_json_file(my_list, "add_item.json")
-
-    print("Arguments added to add_item.json:", arguments)
-
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
