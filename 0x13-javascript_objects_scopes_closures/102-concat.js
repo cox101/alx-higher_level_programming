fd7fd0fa4128fadffe5a376fde4aca78e6b21681
cox101 <cox2659@gmail.com>
2024-03-12 04:56:23 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+    if (err) {
+        console.error(`Error reading ${sourceFile1}: ${err}`);
+        return;
+    }
+
+    fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+        if (err) {
+            console.error(`Error reading ${sourceFile2}: ${err}`);
+            return;
+        }
+
+        const concatenatedData = data1 + data2;
+
+        fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
+            if (err) {
+                console.error(`Error writing to ${destinationFile}: ${err}`);
+                return;
+            }
+            console.log(`Files ${sourceFile1} and ${sourceFile2} concatenated and written to ${destinationFile}`);
+        });
+    });
+});
+
