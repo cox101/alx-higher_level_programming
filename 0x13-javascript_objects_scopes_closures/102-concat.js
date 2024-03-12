8314bfabd4b7de92272cdf1041a7aaf653231f13
cox101 <cox2659@gmail.com>
2024-03-12 05:16:26 -0700

    Initial commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,13 +5,7 @@ const fs = require('fs');
 const contentA = fs.readFileSync(process.argv[2], 'utf8');
 const contentB = fs.readFileSync(process.argv[3], 'utf8');
 
-const contentC = contentA.concat(contentB);
+const concatenatedContent = contentA + contentB;
 
-fs.writeFile(process.argv[4], contentC, 'utf8', function (err) {
-  if (err) {
-    console.error('Error writing file:', err);
-  } else {
-    console.log('Concatenation complete!');
-  }
-});
+fs.writeFileSync(process.argv[4], concatenatedContent);
 
