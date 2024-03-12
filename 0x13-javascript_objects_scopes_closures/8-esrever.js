103581b981f37fe07df0a3069897da0b0d336f47
cox101 <cox2659@gmail.com>
2024-03-12 04:25:51 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+module.exports.esrever = function(list) {
+  let left, right, temp;
+
+  for (left = 0, right = list.length - 1; left < list.length / 2; left++, right--) {
+    temp = list[left];
+    list[left] = list[right];
+    list[right] = temp;
+  }
+
+  return list;
+};
+
