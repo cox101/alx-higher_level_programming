64334a7c1601f99dcad05c9b1e8b2ca4ad4c901d
cox101 <cox2659@gmail.com>
2024-03-12 04:47:01 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const newDict = {};
+
+for (const userId in dict) {
+    const occurrences = dict[userId];
+    if (!newDict[occurrences]) {
+        newDict[occurrences] = [userId];
+    } else {
+        newDict[occurrences].push(userId);
+    }
+}
+
+console.log(newDict);
+
