3a66f78919e1299f073fd3b8cc5c7c91be3e81f8
cox101 <cox2659@gmail.com>
2024-03-12 04:58:43 -0700

    Initial commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,31 +2,16 @@
 
 const fs = require('fs');
 
-const sourceFile1 = process.argv[2];
-const sourceFile2 = process.argv[3];
-const destinationFile = process.argv[4];
+const contentA = fs.readFileSync(process.argv[2], 'utf8');
+const contentB = fs.readFileSync(process.argv[3], 'utf8');
 
-fs.readFile(sourceFile1, 'utf8', (err, data1) => {
-    if (err) {
-        console.error(`Error reading ${sourceFile1}: ${err}`);
-        return;
-    }
+const contentC = contentA.concat(contentB);
 
-    fs.readFile(sourceFile2, 'utf8', (err, data2) => {
-        if (err) {
-            console.error(`Error reading ${sourceFile2}: ${err}`);
-            return;
-        }
-
-        const concatenatedData = data1 + data2;
-
-        fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
-            if (err) {
-                console.error(`Error writing to ${destinationFile}: ${err}`);
-                return;
-            }
-            console.log(`Files ${sourceFile1} and ${sourceFile2} concatenated and written to ${destinationFile}`);
-        });
-    });
+fs.writeFile(process.argv[4], contentC, 'utf8', function (err) {
+  if (err) {
+    console.error('Error writing file:', err);
+  } else {
+    console.log('Concatenation complete!');
+  }
 });
 
