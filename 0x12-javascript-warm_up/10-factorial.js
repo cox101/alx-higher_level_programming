4fd6c636c09ddffb1b0911398e7939ca5cfea24f
cox101 <cox2659@gmail.com>
2024-03-13 12:32:50 -0700

    Initial commit

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
 
-console.log(factorial(parseInt(process.argv[2])));
-
-function factorial (p1) {
-  if (p1 === 0 || p1 === 1 || isNaN(p1)) { return 1; }
-
-  return p1 * factorial(p1 - 1);
+function factorial(n) {
+  if (n < 0) {
+    return -1; // Negative numbers don't have factorials, returning -1 as an indicator
+  }
+  if (n === 0 || isNaN(n)) {
+    return 1; // Factorial of 0 or NaN is 1
+  }
+  return n * factorial(n - 1); // Recursive call to compute factorial
 }
+
+const input = Number(process.argv[2]); // Parse the input from command line arguments
+
+console.log(factorial(input)); // Calculate and print the factorial
+
