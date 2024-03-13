f83a808137c9bcdc0ccbd1e336d902cd249512eb
cox101 <cox2659@gmail.com>
2024-03-13 12:20:25 -0700

    commit

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,24 +1,12 @@
 #!/usr/bin/node
 
-const computeFactorial = (n) => {
-  // Base case: if n is NaN or less than 0, return 1
-  if (isNaN(n) || n < 0) {
-    return 1;
+function factorial(p1) {
+  if (p1 === 0 || p1 === 1 || isNaN(p1)) { 
+    return 1; 
   }
-  // Base case: if n is 0, return 1 (factorial of 0 is defined as 1)
-  if (n === 0) {
-    return 1;
-  }
-  // Recursive case: compute factorial recursively
-  return n * computeFactorial(n - 1);
-};
 
-// Parsing the first argument as an integer
-const num = parseInt(process.argv[2]);
+  return p1 * factorial(p1 - 1);
+}
 
-// Computing the factorial
-const factorial = computeFactorial(num);
-
-// Printing the factorial
-console.log(factorial);
+console.log(factorial(parseInt(process.argv[2])));
 
