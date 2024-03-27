e822f76cf7926a2b24189dd1b15b3b0cfdcfd754
cox101 <cox2659@gmail.com>
2024-03-27 14:01:34 -0700

    Initial commit

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Send a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing "You got me!", in the body of the response
-curl -s -X PUT -L --data "user_id=98" 0.0.0.0:5000/catch_me
-
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool" -d "user_id=98"
