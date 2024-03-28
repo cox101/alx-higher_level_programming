8b261152273ba6cb66a6a58936758b2538d02dd8
cox101 <cox2659@gmail.com>
2024-03-28 14:39:07 -0700

    Initial commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
-from urllib import request
+"""Script that fetches 'https://alx-intranet.hbtn.io/status'"""
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        data = response.read()
-        print("Body response:")
-        print("\t- type:", type(data))
-        print("\t- content:", data)
-        print("\t- utf8 content:", data.decode(encoding='utf-8'))
+    import urllib.request
 
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+    
+    print("Body response:")
+    print('\t- type:', type(html))
+    print('\t- content:', html)
+    print('\t- utf8 content:', html.decode('UTF8'))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import requests
+"""
+Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./5-hbtn_header.py <URL>
+"""
 import sys
+import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
     response = requests.get(url)
-    request_id = response.headers.get('X-Request-Id')
-
-    print(request_id)
+    print(response.headers.get("X-Request-Id"))
 
