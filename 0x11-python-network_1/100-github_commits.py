6593336f85bb071da12b8e83ac8e154663c75aa2
cox101 <cox2659@gmail.com>
2024-03-28 14:19:55 -0700

    Initial commit'

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-import requests
+"""
+Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
 
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
     response = requests.get(url)
+    commits = response.json()
 
-    if response.status_code == 200:
-        commits = response.json()[:10]  # Get the first 10 commits
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    else:
-        print("Error:", response.status_code)
+    for commit in commits[:10]:
+        print(f"{commit['sha']}: {commit['commit']['author']['name']}")
 
