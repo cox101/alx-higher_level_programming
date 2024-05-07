7990d975fd4bce14dae5e98b2f08c8f7cac5fdc4
cox101 <coxb2659@gmail.com>
2024-05-07 14:41:11 -0700

    Commit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,1 +1,24 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_list_info(PyObject *p) {
+    PyListObject *list;
+    Py_ssize_t size, alloc, i;
+
+    if (!PyList_Check(p)) {
+        fprintf(stderr, "Invalid Python list\n");
+        return;
+    }
+
+    list = (PyListObject *)p;
+    size = PyList_Size(p);
+    alloc = list->allocated;
+
+    printf("[*] Size of the Python List = %zd\n", size);
+    printf("[*] Allocated = %zd\n", alloc);
+
+    for (i = 0; i < size; i++) {
+        printf("Element %zd: %s\n", i, Py_TYPE(list->ob_item[i])->tp_name);
+    }
+}
 
